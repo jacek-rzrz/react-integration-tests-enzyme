7941a0d485ac627d08269b8cb6d559bbb950c730
Jacek Rzeniewicz <jrzeniewicz@pivotal.io>
2017-09-12 14:02:43 +0100

    Make the App a named export

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './styles/logo.svg';
 import './styles/App.css';
 
-class App extends Component {
+export class App extends Component {
   render() {
     return (
       <div className="App">
@@ -17,5 +17,3 @@ class App extends Component {
     );
   }
 }
-
-export default App;
