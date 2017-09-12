183a94737995a881e74384da1c6e44f94ca3b5a0
Jacek Rzeniewicz <jrzeniewicz@pivotal.io>
2017-09-12 12:19:32 +0100

    Add enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import { mount } from 'enzyme';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const app = mount(<App />);
+  expect(app).toBePresent();
 });
